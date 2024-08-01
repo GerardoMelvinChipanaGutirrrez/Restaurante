c444ed1744a0ad1fc3c187aee9c9bef944fee834
GerardoMelvinChipanaGutirrrez <90343637+GerardoMelvinChipanaGutirrrez@users.noreply.github.com>
2024-08-01 01:48:44 -0500

    first commit

---
+++ src/js/HeadFoot.js
@@ -0,0 +1,15 @@
+
+
+fetch('/src/pages/Header.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('header-container').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading HTML:', error));
+            
+fetch('/src/pages/Footer.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('footer-container').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading HTML:', error));
